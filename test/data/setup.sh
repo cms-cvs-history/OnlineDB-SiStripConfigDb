502430bca6cbf8b41dadf41a4aeb9ce85d35fccf
cmsbuild <>
2008-04-21 03:14:43 +0000

    This commit was manufactured by cvs2git to create tag 'OnlineDB- SiStripConfigDb-V03-00-02'.
    Sprout from master 2008-03-26 10:08:52 UTC Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch> 'using old db model'
    Cherrypick from master 2008-04-21 03:14:41 UTC Rick Wilkinson <sha1-197b93d87bf2e1eb4349df76c6ec25fd8f1f348e@cern.ch> 'second run of translating configs':
        python/SiStripConfigDb_cfi.py
        test/data/setup.sh
        test/data/tkonlinesw

---
+++ test/data/setup.sh
@@ -0,0 +1,9 @@
+eval `scramv1 runtime -sh`
+scramv1 tool remove tkonlinesw
+scramv1 setup tkonlinesw 0.3-cms file:$CMSSW_BASE/src/OnlineDB/SiStripConfigDb/test/data/tkonlinesw
+eval `scramv1 runtime -sh`
+cd $CMSSW_BASE/src/OnlineDB/SiStripConfigDb/interface/
+replace "//#define USING_NEW_DATABASE_MODEL" "#define USING_NEW_DATABASE_MODEL" -- SiStripConfigDb.h
+cd $CMSSW_BASE/src/OnlineDB/SiStripConfigDb/interface/
+replace "//#define USING_DATABASE_CACHE" "#define USING_DATABASE_CACHE" -- SiStripConfigDb.h
+cd -
