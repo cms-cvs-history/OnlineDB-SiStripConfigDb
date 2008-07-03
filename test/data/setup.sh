27d22e792830617f0ae683cd8687ce62bfa0b731
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2008-07-03 09:30:24 +0000

    removed obsolete files

--- test/data/setup.sh
+++
@@ -1,7 +0,0 @@
-eval `scramv1 runtime -sh`
-scramv1 tool remove tkonlinesw
-scramv1 setup tkonlinesw 0.3-cms file:$CMSSW_BASE/src/OnlineDB/SiStripConfigDb/test/data/tkonlinesw
-eval `scramv1 runtime -sh`
-cd $CMSSW_BASE/src/OnlineDB/SiStripConfigDb/interface/
-replace "//#define USING_NEW_DATABASE_MODEL" "#define USING_NEW_DATABASE_MODEL" -- SiStripConfigDb.h
-cd -
