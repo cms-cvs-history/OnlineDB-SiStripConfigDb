08a460ffb85cff474be0482fb97718002b66ad01
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2008-11-28 16:45:21 +0000

    redefined partition level cfg slightly to ease use for o2o

--- python/SiStripPartitions_cff.py
+++ python/SiStripPartitions_cff.py
@@ -6,26 +6,30 @@ SiStripPartitions = cms.PSet(
     
         PartitionName = cms.untracked.string(''),
 
-        # Overrides run number AND versions 
+	# Forces versions to be taken from "current state". Overrides ALL settings below.
         ForceCurrentState = cms.untracked.bool(False),
 
-        # Null value means use "latest run"
+	# 1) A non-zero value means "retrieve versions from run table".
+	# 2) A null value means "use versions listed below". {0,0} means "current state".
         RunNumber = cms.untracked.uint32(0),
 
-        # Overrides run number and uses versions below
+	# Only "active" with a non-zero run number.
+	# 1) A non-zero version means "override version retrieved from run table".
+	# 2) A null value {0,0} means "do not override versions retrieved from run table".
         ForceVersions = cms.untracked.bool(False),
 
-        # Syntax is {major,minor}. (0,0) means "current state". 
+	# Syntax is {major,minor}. 
         CablingVersion   = cms.untracked.vuint32(0,0),
         FecVersion       = cms.untracked.vuint32(0,0),
         FedVersion       = cms.untracked.vuint32(0,0),
         DcuDetIdsVersion = cms.untracked.vuint32(0,0),
         DcuPsuMapVersion = cms.untracked.vuint32(0,0),
 
-        # Null value means use (analysis) versions below ("calibration")
+        # A non-zero number means "retrieve a complete calibration set".
+        # A null value means "use analysis versions below". {0,0} means "current state".
         GlobalAnalysisVersion = cms.untracked.uint32(0),
 
-        # Syntax is {major,minor}. (0,0) means "current state". ("history")
+	# Syntax is {major,minor}. 
         FastCablingVersion = cms.untracked.vuint32(0,0),
         ApvTimingVersion   = cms.untracked.vuint32(0,0),
         OptoScanVersion    = cms.untracked.vuint32(0,0),
